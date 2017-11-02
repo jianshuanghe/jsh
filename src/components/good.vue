cb82d3a77ed327d54fef3be7e54f2cfeb8b572d6
jianshuanghe <274088960@qq.com>
2017-11-02 13:37:22 +0800

    first commit

--- src/components/good.vue
+++ src/components/good.vue
@@ -11,6 +11,7 @@
       <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
     </ul>
     <h2>Ecosystem</h2>
+    <x-button slot="right" type="primary" action-type="button" mini @click="sendMessage">{{word}}</x-button>
     <ul>
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
@@ -25,10 +26,29 @@ export default {
   name: 'Good',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      word: '发送验证码',
+      isOvertime: false
     }
   },
   methods: {
+    sendMessage(){
+      if(this.isOvertime){
+          return false;
+      }
+      let that = this,
+          time = 60;
+      var sendTimer = setInterval(function(){
+          that.isOvertime = true;
+          time--;
+          that.word = "重新发送"+time;
+          if(time < 0){
+              that.isOvertime = false;
+              clearInterval(sendTimer);
+              that.word = "获取验证码";
+          }
+      },1000)
+    },
     applyFor () {
       this.$router.push('/');
     }
